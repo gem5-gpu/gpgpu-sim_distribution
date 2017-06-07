e83ef133b7a8867fd0fae075bef4eac4e548d0b5
Joel Hestness <jthestness@gmail.com>
2017-06-07 10:11:31 -0500

    shader: Fix REM operation latency
    The modulo operator has the same latency as integer division in Fermi and
    Maxwell hardware. Configure the REM (remainder) instruction consistently with
    DIV.

--- src/cuda-sim/cuda-sim.cc
+++ src/cuda-sim/cuda-sim.cc
@@ -735,6 +735,26 @@ void ptx_instruction::set_opcode_and_latency()
 		   break;
 	   }
 	   break;
+   case REM_OP:
+       // Integer only int div latency
+       op = SFU_OP;
+       switch(get_type()){
+       case F64_TYPE:
+       case FF64_TYPE:
+       case F32_TYPE:
+           panic("REM_OP must be int type, not: %d\n", get_type());
+           break;
+       case B32_TYPE:
+       case U32_TYPE:
+       case S32_TYPE:
+           latency = int_latency[4];
+           initiation_interval = int_init[4];
+           break;
+       default:
+           panic("Unknown REM_OP type: %d\n", get_type());
+           break;
+       }
+       break;
    case SQRT_OP: case SIN_OP: case COS_OP: case EX2_OP: case LG2_OP: case RSQRT_OP: case RCP_OP:
 	   //Using double to approximate those
 	  latency = dp_latency[2];
